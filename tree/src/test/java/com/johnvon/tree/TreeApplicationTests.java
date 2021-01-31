bf79e174a4e61b54e61ae7dbbf18d7f7542397fb
johnvon <309529361@qq.com>
2021-01-31 22:49:22 +0800

    初始化

---
+++ tree/src/test/java/com/johnvon/tree/TreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnvon.tree;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
